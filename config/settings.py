134edbe894ece0d4a76214c00e6cd98028dd60f7
haruka <nekonekonyanko333@gmail.com>
2025-09-06 11:32:46 +0900

    商品一覧、詳細機能実装

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products'
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'config/templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -116,9 +117,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
---
+++ products/views.py
@@ -0,0 +1,20 @@
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Product
+# from django.http import HttpResponse
+
+from django.shortcuts import render
+
+# Create your views here.
+class ProductListView(ListView):
+  model = Product
+  template_name = "products/product_list.html"
+
+class ProductDetailView(DetailView):
+  model = Product
+  template_name = "products/product_detail.html"
+
+  def get_context_data(self,**kwargs):
+    context = super().get_context_data(**kwargs)
+    context["related_products"] = Product.objects.order_by('-created_at')
+    return context
